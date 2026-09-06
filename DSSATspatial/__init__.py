4264eaa0aee88fcd4710343976929af0763ebef1
ssakthi888 <165066244+ssakthi888@users.noreply.github.com>
2026-09-06 09:05:35 +0530

    modified

--- DSSATspatial/__init__.py
+++ DSSATspatial/__init__.py
@@ -6,18 +6,21 @@ import shutil
 import tempfile
 import threading
 import warnings
+import inspect
 
-# 1. Dynamic Environment Setup
-_exe_name = os.environ.get('DSSAT_EXE', 'dscsm0485.exe')
-os.environ['DSSAT_EXE'] = _exe_name
-_version_match = re.search(r'\d+', _exe_name)
-if _version_match:
-    os.environ['DSSAT_DATA'] = f"Data_{int(_version_match.group())}"
+# 1. Dynamic Environment Setup via Caller's Globals
+# Peek into the notebook/script that is executing the import statement
+_caller_globals = inspect.currentframe().f_back.f_globals
+
+_dssat_version = _caller_globals.get('DSSAT_VERSION', '4.8.5')
+_clean_version = _dssat_version.replace('.', '')
+
+os.environ['DSSAT_EXE'] = f"dscsm0{_clean_version}.exe"
+os.environ['DSSAT_DATA'] = f"Data_{_clean_version}"
 
 # 2. Configure Temp Directory Dynamically
-# Defaults to standard system temp, but accepts user overrides from os.environ
 _system_temp = os.path.join(tempfile.gettempdir(), "DSSAT_Batch_Temp")
-_custom_temp = os.environ.get('DSSAT_CUSTOM_TEMP', _system_temp)
+_custom_temp = _caller_globals.get('CUSTOM_TEMP', _system_temp)
 
 if os.path.exists(_custom_temp):
     shutil.rmtree(_custom_temp, ignore_errors=True)
@@ -50,9 +53,6 @@ VERSION = '048'
 from . import crop
 from .soil import SoilProfile, SoilLayer
 from .weather import WeatherStation, WeatherRecord
-from .run import DSSAT
+from .run import DSSAT, BIN_PATH, STATIC_PATH
 from . import filex
-
-__all__ = [
-    'crop', 'SoilProfile', 'WeatherStation', 'DSSAT', 'filex'
-]
+from .batch_processor import *
